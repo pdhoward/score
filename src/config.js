2aa48fafa8b754b4c82b216ae2f266858692a1f3
pdhoward <patrick.howard@hotmail.com>
2019-03-14 19:31:58 -0400

    initialize

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyAC_GKNCe4_CtV6YfpUsmBUxcfQnY5QsaM",
-  clientId:
-    "187565479255-p0ugfjl2n5rf1aguldmsqd16ottr767s.apps.googleusercontent.com",
-  scope: "https://www.googleapis.com/auth/spreadsheets",
-  // spreadsheetId: "1IMyCdzG-3lm6zIcE56LPURJBHuNdr9DatPBNB-pJAeA"
-  spreadsheetId: "1KTArYwDWrn52fnc7B12KvjRb6nmcEaU6gXYehWfsZSo"
-};
